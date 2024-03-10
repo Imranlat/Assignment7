caac92379514d6a3a555106815b750223f2d41af
Imran latif <159417772+Imranlat@users.noreply.github.com>
2024-03-10 12:27:26 +0500

    Add files via upload

---
+++ q7.js
@@ -0,0 +1,6 @@
+"use strict";
+//7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log("Addition:", 4 + 4);
+console.log("Subtraction:", 18 - 10);
+console.log("Multiplication:", 8 * 1);
+console.log("Division:", 16 / 2);
